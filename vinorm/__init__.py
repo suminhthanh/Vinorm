13770e7c117387a9211574a9850759b4d02df159
tsu <smthanh@gmail.com>
2024-05-10 16:01:17 +0700

    Update __init__.py

--- vinorm/__init__.py
+++ vinorm/__init__.py
@@ -25,7 +25,7 @@ def TTSnorm(text, punc = False, unknown = True, lower = True, rule = False ):
     subprocess.check_call(Command, env=myenv, cwd=A)
     
     O=A+"/output.txt"
-    with open(O,"r") as fr:
+    with open(O,"r", encoding='utf-8') as fr:
         text=fr.read()
     TEXT=""
     S=text.split("#line#")
